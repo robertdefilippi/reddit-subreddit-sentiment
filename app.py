52ba5d839d78b292422f52d26262b588b61acd1a
robertdefilippi <robert.defilippi@gmail.com>
2019-12-07 11:24:33 -0500

    Changed app run

--- app.py
+++ app.py
@@ -279,5 +279,4 @@ if __name__ == "__main__":
     app.secret_key = os.urandom(12)
     # app.run(debug=True, host="localhost", port=8000)
     init_scheduler()
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port, threaded=True)
+    app.run()
