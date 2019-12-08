5a943103b9f882b7c6798e1bbd8131b83d5c81a1
robertdefilippi <robert.defilippi@gmail.com>
2019-12-08 09:56:22 -0500

    Update table structure:

--- sql/create_sentiment.sql
+++ sql/create_sentiment.sql
@@ -1,8 +1,15 @@
-CREATE TABLE sentiment (
-post_ts timestamp NOT NULL,
-subreddit VARCHAR NOT NULL,
-post_title VARCHAR NOT NULL,
-score_negative FLOAT NOT NULL,
-score_neutral FLOAT NOT NULL,
-score_positive FLOAT NOT NULL,
-score_compound FLOAT NOT NULL);
+create table sentiment
+(
+  id serial NOT NULL,
+  post_ts timestamp NOT NULL,
+  subreddit VARCHAR NOT NULL,
+  post_title VARCHAR NOT NULL,
+  score_negative FLOAT NOT NULL,
+  score_neutral FLOAT NOT NULL,
+  score_positive FLOAT NOT NULL,
+  score_compound FLOAT NOT NULL
+);
+
+create unique index sent_2_id_uindex
+	on sent_2 (id);
+
